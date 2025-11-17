972c5ad10ccac39e00c41249d3ba0dac907cca95
Don Williams <don.e.williams@gmail.com>
2025-11-17 10:13:15 -0500

    Moved fonts to fonts.nix

--- configuration.nix
+++ configuration.nix
@@ -4,6 +4,7 @@
   imports =
     [
       ./hardware-configuration.nix
+      ./fonts.nix
     ];
 
   boot = {
@@ -140,33 +141,6 @@
     yazi
     zig
   ];
-  fonts = {
-    packages = with pkgs; [
-      dejavu_fonts
-      fira-code
-      fira-code-symbols
-      font-awesome
-      hackgen-nf-font
-      ibm-plex
-      inter
-      jetbrains-mono
-      material-icons
-      maple-mono.NF
-      minecraftia
-      nerd-fonts.im-writing
-      nerd-fonts.blex-mono
-      noto-fonts
-      noto-fonts-color-emoji
-      noto-fonts-cjk-sans
-      noto-fonts-cjk-serif
-      noto-fonts-monochrome-emoji
-      powerline-fonts
-      roboto
-      roboto-mono
-      symbola
-      terminus_font
-    ];
-  };
 
   systemd.services.flatpak-add-flathub = {
     description = "Add Flathub Flatpak remote";
